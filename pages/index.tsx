dbc22e6661f6331720818192a44cff792bbcb719
Raafi Muhammad <rafmhmd06@gmail.com>
2020-11-08 10:56:02 +0700

    Improve foldering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import moment from 'moment';
 import Head from 'next/head';
 import React from 'react';
-import MainLayout from '../component/layouts/main';
-
+import MainLayout from '../src/component/layouts/main';
 
 export default class Index extends React.Component {
 
