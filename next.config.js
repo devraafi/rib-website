f0d02a519c054f78048a9d300e3ce08fd6918cd2
Raafi Muhammad <rafmhmd06@gmail.com>
2020-11-08 13:12:09 +0700

    tes deui

--- next.config.js
+++ next.config.js
@@ -1,42 +1,40 @@
 // const withCss = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass');
-const optimizedImages = require('next-optimized-images');
+// const optimizedImages = require('next-optimized-images');
 const withBabelMinify = require('next-babel-minify')();
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const withTypescript = require('@zeit/next-typescript');
 
 module.exports =
-  optimizedImages(
-    withBabelMinify(
-      withSass(
-        withTypescript(
-          {
-            webpack: function (config) {
-              config.module.rules.push({
-                test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-                use: {
-                  loader: 'url-loader',
-                  options: {
-                    limit: 100000,
-                    name: '[name].[ext]'
-                  }
+  withBabelMinify(
+    withSass(
+      withTypescript(
+        {
+          webpack: function (config) {
+            config.module.rules.push({
+              test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+              use: {
+                loader: 'url-loader',
+                options: {
+                  limit: 100000,
+                  name: '[name].[ext]'
                 }
-              });
-              config.optimization.minimizer.push(
-                new OptimizeCSSAssetsPlugin({})
-              )
-              return config
-            },
-            /* config options here */
-            minified: true,
-            cssLoaderOptions: {
-              importLoaders: 1,
-              localIdentName: "[local]___[hash:base64:5]",
-            },
+              }
+            });
+            config.optimization.minimizer.push(
+              new OptimizeCSSAssetsPlugin({})
+            )
+            return config
           },
-        ), {
-        sassLoaderOptions: {}
-      }
-      )
+          /* config options here */
+          minified: true,
+          cssLoaderOptions: {
+            importLoaders: 1,
+            localIdentName: "[local]___[hash:base64:5]",
+          },
+        },
+      ), {
+      sassLoaderOptions: {}
+    }
     )
   )
