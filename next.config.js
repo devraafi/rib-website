76cf96e67053315d4c80a932e8d16768820ffe9f
Raafi Muhammad <rafmhmd06@gmail.com>
2020-11-08 14:31:52 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -8,33 +8,32 @@ const withTypescript = require('@zeit/next-typescript');
 module.exports =
   withBabelMinify(
     withSass(
-      withTypescript(
-        {
-          webpack: function (config) {
-            config.module.rules.push({
-              test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-              use: {
-                loader: 'url-loader',
-                options: {
-                  limit: 100000,
-                  name: '[name].[ext]'
-                }
+      {
+        webpack: function (config) {
+          config.module.rules.push({
+            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 100000,
+                name: '[name].[ext]'
               }
-            });
-            config.optimization.minimizer.push(
-              new OptimizeCSSAssetsPlugin({})
-            )
-            return config
-          },
-          /* config options here */
-          minified: true,
-          cssLoaderOptions: {
-            importLoaders: 1,
-            localIdentName: "[local]___[hash:base64:5]",
-          },
+            }
+          });
+          config.optimization.minimizer.push(
+            new OptimizeCSSAssetsPlugin({})
+          )
+          return config
         },
-      ), {
-      sassLoaderOptions: {}
-    }
+        /* config options here */
+        minified: true,
+        cssLoaderOptions: {
+          importLoaders: 1,
+          localIdentName: "[local]___[hash:base64:5]",
+        },
+      },
+      {
+        sassLoaderOptions: {}
+      }
     )
   )
