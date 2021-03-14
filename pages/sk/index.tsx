b12d060dda9aa956f78cfa35735def1544346d32
Raafi Muhammad <rafmhmd06@gmail.com>
2021-03-14 13:03:55 +0700

    sljdksjd

---
+++ pages/sk/index.tsx
@@ -0,0 +1,8 @@
+import SkComponent from "@Components/main/sk/sk.component";
+import React from "react";
+
+const Index = () => (
+    <SkComponent />
+)
+
+export default Index;
---
+++ src/components/main/sk/sk.component.tsx
@@ -0,0 +1,76 @@
+import MainComponent from '@Components/layout/main/main-layout.component';
+import React from 'react';
+
+const SkComponent = () => (
+    <MainComponent
+        title="Syarat & Ketentuan | Ruang Insan Berbagi"
+        description="Ruang Insan Berbagi"
+        pageId="sk-page-dh"
+    >
+        <div className="container-lg sk-wrapper py-lg-5 py-4">
+            <div className="title col-lg-5 m-auto text-center">
+                Syarat & Ketentuan
+            </div>
+            <div className="py-3 col-lg-3 m-auto">
+                <div className="separatore"></div>
+            </div>
+            <div className="content py-3">
+                <div className="subtitle mb-3">Purus ut vitae, aenean vel. Gravida eleifend sed</div>
+                <ol type="1">
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                </ol>
+            </div>
+            <div className="content">
+                <div className="subtitle mb-3">Purus ut vitae, aenean vel. Gravida eleifend sed</div>
+                <ul>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed</li>
+                    <ol type="1">
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    </ol>
+                    <li>Purus ut vitae, aenean vel. Gravida eleifend sed</li>
+                    <ol type="1">
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                        <li>Purus ut vitae, aenean vel. Gravida eleifend sed.</li>
+                    </ol>
+                </ul>
+            </div>
+        </div>
+    </MainComponent>
+)
+
+export default SkComponent;
