81938c9acc050f5aa2a70541298129bb82459e80
Raafi Muhammad <rafmhmd06@gmail.com>
2021-05-02 15:15:33 +0700

    tes

--- src/services/auth/aut.service.ts
+++ src/services/auth/aut.service.ts
@@ -12,6 +12,7 @@ export class AuthenticationService {
         const currentUser = local ? local.getItem('userInfo') : {};
 
         // checking if user manually insert the localstorage
+        
         let theUser;
         try {
             theUser = JSON.parse(JSON.stringify(currentUser));
