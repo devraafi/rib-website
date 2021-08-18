ba4de110d3a7f9894d735ee277e0f5659bc7c66e
Raafi Muhammad <rafmhmd06@gmail.com>
2021-08-18 11:17:59 +0700

    tes meta 1

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const prod = process.env.NODE_ENV === 'production';
 module.exports = {
   env: {
     staging: prod ? 'https://rib-production.ruanginsanberbagi.org/' : 'https://rib-production.ruanginsanberbagi.org/',
-    baseUrl: prod ? 'https://ruanginsanberbagi.org/' : 'http://4068a9add13b.ngrok.io/'
+    baseUrl: prod ? 'https://ruanginsanberbagi.org/' : 'https://rib-website-green.vercel.app/'
   },
   externals: {
     'react': 'React'
