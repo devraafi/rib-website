f173d34b0f7c7e2a24554274a601ab564ac0162b
Raafi Muhammad <rafmhmd06@gmail.com>
2021-08-18 12:51:44 +0700

    vercel

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const prod = process.env.NODE_ENV === 'production';
 module.exports = {
   env: {
     staging: prod ? 'https://rib-production.ruanginsanberbagi.org/' : 'https://rib-production.ruanginsanberbagi.org/',
-    baseUrl: prod ? 'https://ruanginsanberbagi.org/' : 'https://cowardly-penguin-2.loca.lt/'
+    baseUrl: prod ? 'https://rib-website-green.vercel.app/' : 'https://rib-website-green.vercel.app/'
   },
   externals: {
     'react': 'React'
